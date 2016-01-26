4f99d4b2990ca7998c8dd50b7b5cab99d77b7032
Magesh K <mgshk.1507@gmail.com>
2016-01-26 21:51:11 +0530

    Added bower components

---
+++ js/app.js
@@ -0,0 +1,14 @@
+var app = angular.module('app', [
+	'ui.router'
+]);
+
+app.config(function($stateProvider, $urlRouterProvider) {
+    
+    $urlRouterProvider.otherwise('/home');
+    
+    $stateProvider
+        .state('/home', {
+            url: '/home',
+            templateUrl: 'templates/home.html'
+        }); 
+});
