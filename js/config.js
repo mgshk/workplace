ff20ffc1382e0482c0dbea5af0677ec1647cfe69
Nagamurali <nagamuraliweb@gmail.com>
2016-03-19 16:29:01 +0530

    deign changes and config setup

--- js/config.js
+++ js/config.js
@@ -1,26 +1,121 @@
-angular.module('app.controllers', [
-	'app.controller.benefitsCtrl',
-	'app.controller.birthdayCtrl',
-	'app.controller.compensationCtrl',
-	'app.controller.dashboardCtrl',
-	'app.controller.directoryCtrl',
-	'app.controller.expensesCtrl',
-	'app.controller.favoritiesCtrl',
-	'app.controller.homeCtrl',
-	'app.controller.loginCtrl',
-	'app.controller.forgetPasswordCtrl',
-	'app.controller.signUpCtrl',
-	'app.controller.myTeamCtrl',
-	'app.controller.payCtrl',
-	'app.controller.personalInformationCtrl',
-	'app.controller.teamPerformanceCtrl',
-	'app.controller.timeOffCtrl'
-]);
+angular.module('app.configs', ['ui.router'])
+	.config(function($stateProvider, $urlRouterProvider, $httpProvider) {
+    
+	$urlRouterProvider.otherwise('/login');
 
-angular.module('app.factories', [
-	'app.factory.loginFactory'
-]);
-
-angular.module('app.services', [
-	'app.service.loginService'
-]);
+	$stateProvider
+		.state('/benefits', {
+			url: '/benefits',
+			templateUrl: 'templates/benefits.html',
+			controller: 'benefitsCtrl',
+			data: {
+			  requiresLogin: true
+			}
+		}).state('/birthday', {
+			url: '/birthday',
+			templateUrl: 'templates/birthday.html',
+			controller: 'birthdayCtrl',
+			data: {
+			  requiresLogin: true
+			}
+		}).state('/compensation', {
+			url: '/compensation',
+			templateUrl: 'templates/compensation.html',
+			controller: 'compensationCtrl',
+			data: {
+			  requiresLogin: true
+			}
+		}).state('/dashboards', {
+			url: '/dashboards',
+			templateUrl: 'templates/dashboards.html',
+			controller: 'dashboardCtrl',
+			data: {
+			  requiresLogin: false
+			}
+		}).state('/directory', {
+			url: '/directory',
+			templateUrl: 'templates/directory.html',
+			controller: 'directoryCtrl',
+			data: {
+			  requiresLogin: true
+			}
+		}).state('/expenses', {
+			url: '/expenses',
+			templateUrl: 'templates/expenses.html',
+			controller: 'expensesCtrl',
+			data: {
+			  requiresLogin: true
+			}
+		}).state('/favorities', {
+			url: '/favorities',
+			templateUrl: 'templates/favorities.html',
+			controller: 'favoritiesCtrl',
+			data: {
+			  requiresLogin: true
+			}
+		}).state('/home', {
+			url: '/home',
+			templateUrl: 'templates/home.html',
+			controller: 'homeCtrl',
+			data: {
+			  requiresLogin: true
+			}
+		}).state('/login', {
+			url: '/login',
+			templateUrl: 'templates/login.html',
+			controller: 'loginCtrl'
+		}).state('/forget_password', {
+			url: '/forget_password',
+			templateUrl: 'templates/forget_password.html',
+			controller: 'forgetPasswordCtrl'
+		}).state('/signup', {
+			url: '/signup',
+			templateUrl: 'templates/signup.html',
+			controller: 'signUpCtrl'
+		}).state('/my_team', {
+			url: '/my_team',
+			templateUrl: 'templates/my_team.html',
+			controller: 'myTeamCtrl',
+			data: {
+			  requiresLogin: true
+			}
+		}).state('/pay', {
+			url: '/pay',
+			templateUrl: 'templates/pay.html',
+			controller: 'payCtrl',
+			data: {
+			  requiresLogin: true
+			}
+		}).state('/personal_information', {
+			url: '/personal_information',
+			templateUrl: 'templates/personal_information.html',
+			controller: 'personalInformationCtrl',
+			data: {
+			  requiresLogin: true
+			}
+		}).state('/team_performance', {
+			url: '/team_performance',
+			templateUrl: 'templates/team_performance.html',
+			controller: 'teamPerformanceCtrl',
+			data: {
+			  requiresLogin: true
+			}
+		}).state('/time_off', {
+			url: '/time_off',
+			templateUrl: 'templates/time_off.html',
+			controller: 'timeOffCtrl',
+			data: {
+			  requiresLogin: true
+			}
+		});
+}).run(function($rootScope, $state, store, localStorage) {
+	$rootScope.$on('$stateChangeStart', function(e, to) {
+		
+		//if (to.data && to.data.requiresLogin) {
+		//	if (!store.get('userId')) {
+		//		e.preventDefault();
+		//		$state.go('/login');
+		//	}
+		//}
+	});
+});
--- js/services/loginService.js
+++ js/services/loginService.js
@@ -2,19 +2,19 @@ angular.module('app.service.loginService', [])
 	.service('loginService', ['$http', function($http) {
 		
 		this.userLogin = function(user) {
-			return $http.get('', user).then(function (resp) {
+			return $http.post('', user).then(function (resp) {
 				return resp;
 			});		
 		}
 		
 		this.forgotPassword = function(user) {
-			return $http.get('', user).then(function (resp) {
+			return $http.post('', user).then(function (resp) {
 				return resp;
 			});		
 		}
 		
 		this.signUp = function(user) {
-			return $http.get('', user).then(function (resp) {
+			return $http.post('', user).then(function (resp) {
 				return resp;
 			});		
 		}
